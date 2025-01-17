e6dd416c05f09d79956cf521aabef736263317ba
Logan <logan.sauer.012@gmail.com>
2025-01-17 01:18:50 -0500

    Add skull to page and remove spinning box

--- src/app/pottery/page.tsx
+++ src/app/pottery/page.tsx
@@ -1,36 +1,13 @@
 "use client";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { useRef, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Skull } from "../models/skull";
 
 export default function Pottery() {
-  const Box = ({ scale = 1, ...props }) => {
-    const ref = useRef({ rotation: { x: 0, y: 0 } });
-    const [hovered, hover] = useState(false);
-    const [clicked, click] = useState(false);
-    useFrame((state, delta) => {
-      ref.current.rotation.x += delta;
-      ref.current.rotation.y += delta;
-    });
-    return (
-      <mesh
-        {...props}
-        ref={ref}
-        scale={(clicked ? 1.5 : 1) * scale}
-        onClick={() => click(!clicked)}
-        onPointerOver={(event) => (event.stopPropagation(), hover(true))}
-        onPointerOut={() => hover(false)}
-      >
-        <boxGeometry />
-        <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
-      </mesh>
-    );
-  };
-
   return (
-    <Canvas>
+    <Canvas style={{ height: "90vh", width: "100vw" }}>
       <ambientLight intensity={0.1} />
       <directionalLight position={[0, 0, 5]} />
-      <Box scale={5} />
+      <Skull />
     </Canvas>
   );
 }
